80a7b5cfd60c509b8b5ee0648adc9c3b5ae107af
dou <ideafrom@163.com>
2017-05-02 19:33:03 +0800

    none

--- dotwoweb/public/js/main.js
+++ dotwoweb/public/js/main.js
@@ -1,5 +1,10 @@
 var HOST = 'http://192.168.10.111:3001';
 
+/************************************************************************************/
+// description: 调用此接口可直接在列表添加新闻内容，初始页面的时候会调用一次
+// count为请求数量， offset为请求起点（偏置值）
+// 该接口会通过ajax得到数据，并调用getANewsHtml以动态生成所需要添加的html代码
+/************************************************************************************/
 var displayNewsList = function (count, offset) {
     // var ajax = getAjaxObject();
     $.get(HOST + "/api/get_news_list?count=" + count + "&begin=" + offset, function(d, status){
@@ -8,11 +13,20 @@ var displayNewsList = function (count, offset) {
             for (var i=0; i<data.length; i++) {
                 var newsHtml = getANewsHtml(data[i]);
                 $('#accordion').append(newsHtml);
+                // 设置监听
+                $('#collapsed_' + data[i].id).click(function () {
+                    var id = $(this).attr('id').substring(10);
+                    drawEmotionDistribution(id);
+                });
             }
         }
     });
 };
 
+/************************************************************************************/
+// description: 生成html代码
+// data为得到的新闻json数据
+/************************************************************************************/
 var getANewsHtml = function (data) {
     var summary = (data.summary ? data.summary : '');
     var imgs = JSON.parse(data.imgs_url);
@@ -33,7 +47,7 @@ var getANewsHtml = function (data) {
                     <a class="remark" href="' + data.url + '">来源: ' + data.origin + '</a>\
                 </div>\
                 <!-- 触发详情页 -->\
-                <a class="collapsed" id="' + data.id + '" role="button" data-toggle="collapse"\
+                <a class="collapsed" id="collapsed_' + data.id + '" role="button" data-toggle="collapse"\
                     data-parent="#accordion" href="#collapse_' + data.id + '" aria-controls="collapse_' + data.id + '"\
                     aria-expanded="false">\
                         <h5 class="media-heading">\
@@ -51,15 +65,29 @@ var getANewsHtml = function (data) {
                 </h4>\
                 </div>\
                 <!-- 下拉详情页 -->\
-                <div id="collapse_' + data.id + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_' + data.id + '">\
-                    <div class="panel-body sub_panel">\
-                    <div class="panel panel-primary">\
-                    <div class="panel-heading">社会效应</div>\
-                    <div class="panel-body no_space">\
-                    <!-- 为ECharts准备一个具备大小（宽高）的Dom -->\
-                <div id="echarts_main" style="height:300px;"></div>\
+                <div id="collapse_' + data.id + '" news_id = "' + data.id + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_' + data.id + '">\
+                     <ul class="nav nav-tabs collapse_nav" role="tablist">\
+                <li role="presentation" class="active"><a href="#emotion" aria-controls="emotion" role="tab" data-toggle="tab">情感分布</a></li>\
+                <li role="presentation"><a href="#emotion-locaton" aria-controls="emotion-locaton" role="tab" data-toggle="tab">情感-地域</a></li>\
+                <li role="presentation"><a href="#localtion" aria-controls="localtion" role="tab" data-toggle="tab">地域分布</a></li>\
+                </ul>\
+                <!-- Tab panes -->\
+                 <div class="tab-content">\
+                    <div role="tabpanel" class="tab-pane active" id="emotion">\
+                        <!-- 为ECharts准备一个具备大小（宽高）的Dom -->\
+                        <div id="echarts_emotion_' + data.id + '"></div>\
+                    </div>\
+                    <div role="tabpanel" class="tab-pane" id="emotion-locaton">\
+                        <!-- 为ECharts准备一个具备大小（宽高）的Dom -->\
+                        <div id="echarts_2_"' + data.id + '></div>\
+                    </div>\
+                    <div role="tabpanel" class="tab-pane" id="localtion">\
+                        <!-- 为ECharts准备一个具备大小（宽高）的Dom -->\
+                        <div id="echarts_3_"' + data.id + '></div>\
+                    </div>\
+            <!-- end of Tab panes -->\
             </div>\
-            </div>\
+               <!-- end of 下拉详情页 -->\
             </div>\
             </div>\
             </div>\
@@ -67,59 +95,71 @@ var getANewsHtml = function (data) {
     return html;
 };
 
+/************************************************************************************/
+// description: 调用此接口以绘制情感分布图
+/************************************************************************************/
 var drawEmotionDistribution = function (id) {
-    // 路径配置
-    require.config({
-        paths: {
-            echarts: '../static/echarts'
-        }
-    });
-    // 使用
-    require(
-        [
-            'echarts',
-            'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
-        ],
-        function (ec) {
-            // 基于准备好的dom，初始化echarts图表
-            var myChart = ec.init(document.getElementById('echarts_main'));
-
-            var option = {
-                tooltip: {
-                    show: true
-                },
-                legend: {
-                    data: ['情感分布']
-                },
-                xAxis: [
-                    {
-                        type: 'category',
-                    },
+    $('#echarts_emotion_' + id).attr("style", "height:300px; width:" + document.body.clientWidth/1.05 + "px");
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('echarts_emotion_' + id));
+    // myChart.showLoading();
+    // myChart.hideLoading();
+    option = {
+        title : {
+            text: '情感分布',
+            subtext: '',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['喜悦','悲伤','恐惧','愤怒']
+        },
+        series : [
+            {
+                name: '评论数量',
+                type: 'pie',
+                radius : '55%',
+                center: ['50%', '60%'],
+                data:[
+                    {value:2252, name:'喜悦'},
+                    {value:6421, name:'悲伤'},
+                    {value:4356, name:'恐惧'},
+                    {value:5412, name:'愤怒'}
                 ],
-                yAxis: [
-                    {
-                        type: 'value'
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
                     }
-                ],
-                series: [
-                    {
-                        "name": "情感分布",
-                        "type": "bar",
-                        "data": [229, 1203, 3091, 872]
-                    }
-                ]
-            };
-            myChart.setOption(option);
-        }
-    );
-}
+                }
+            }
+        ]
+    };
+    myChart.setOption(option);
+};
+
+/************************************************************************************/
+// description: 绘制地域分布图
+/************************************************************************************/
+var drawEmotionLocation = function (id) {
+    $('#echarts_emotion_' + id).attr("style", "height:300px; width:" + document.body.clientWidth/1.1 + "px");
+    var myChart = echarts.init(document.getElementById('echarts_emotion_' + id));
+    var option = {};
+    myChart.setOption(option);
+};
 
 
-// 为echarts对象加载数据 
-// $('#collapseOneTrigger').click(function () {
-//     setTimeout(drawEmotionDistribution, 100);
-// });
 
+
+/************************************************************************************/
+// description: 入口
+/************************************************************************************/
 var offset = 0;
 var loadCount = 10;
 displayNewsList(loadCount, offset);
